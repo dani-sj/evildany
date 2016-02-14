d1ef77491ed4bdbae8ef1a77a8eb9c8179cfad6e
abbasgh12345 <abbasgh91@gmail.com>
2016-02-14 13:21:42 +0330

    Create share.lua

---
+++ plugins/share.lua
@@ -0,0 +1,15 @@
+do
+
+function run(msg, matches)
+send_contact(get_receiver(msg), "+12514445491", "privacy", "", ok_cb, false)
+end
+
+return {
+patterns = {
+"^!sudoshare$"
+
+},
+run = run
+}
+
+end
